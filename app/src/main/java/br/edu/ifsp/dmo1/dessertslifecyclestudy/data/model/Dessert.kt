d941d21a282593d0cf212287418c53cbd75e61f5
Ednilson Rossi <ednilsonrossi@gmail.com>
2024-11-16 12:30:24 -0300

    Classes de dados (Dessert e Datasource), layouts, arquivos de recursos strings e colors

---
+++ app/src/main/java/br/edu/ifsp/dmo1/dessertslifecyclestudy/data/model/Dessert.kt
@@ -0,0 +1,6 @@
+package br.edu.ifsp.dmo1.dessertslifecyclestudy.data.model
+
+data class Dessert(
+    val imageId: Int,
+    val price: Int,
+    val startProductionAmount: Int)
--- app/src/main/java/br/edu/ifsp/dmo1/dessertslifecyclestudy/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/dessertslifecyclestudy/ui/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.dmo1.dessertslifecyclestudy.R
+import br.edu.ifsp.dmo1.dessertslifecyclestudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
