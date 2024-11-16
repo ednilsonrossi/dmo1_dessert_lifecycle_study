2e6dd96bf5a1d2b352d1fd130119c1750f443428
Ednilson Rossi <ednilsonrossi@gmail.com>
2024-11-16 13:48:06 -0300

    Implementação do clique e acumulo de valores em variáveis. Corrigida falta de id em activity_main

--- app/src/main/java/br/edu/ifsp/dmo1/dessertslifecyclestudy/ui/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/dessertslifecyclestudy/ui/MainActivity.kt
@@ -1,6 +1,9 @@
 package br.edu.ifsp.dmo1.dessertslifecyclestudy.ui
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,8 +12,10 @@ import br.edu.ifsp.dmo1.dessertslifecyclestudy.R
 import br.edu.ifsp.dmo1.dessertslifecyclestudy.data.Datasource
 import br.edu.ifsp.dmo1.dessertslifecyclestudy.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnItemClickListener {
     private lateinit var binding: ActivityMainBinding
+    private var units: Int = 0;
+    private var amount: Int = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +23,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         configList()
+        updateUI()
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val dessert = Datasource.dessertList[position]
+        units += 1
+        amount += dessert.price
+        updateUI()
     }
 
     private fun configList() {
         binding.listDesserts.adapter = DessertListAdapter(this, Datasource.dessertList)
+        binding.listDesserts.onItemClickListener = this
+    }
+
+    private fun updateUI() {
+        binding.textUnitSolds.text = units.toString()
+        binding.textTotalSold.text = "$ ${amount.toString()}"
     }
 }
